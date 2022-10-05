ea3d06829463e1cc6143b9e0e8010f05f4890f48
kellychan56 <kelly.chan568@gmail.com>
2022-10-05 10:38:09 -0700

    working validation test for song requiring a user

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
    belongs_to :user
-   validates :title, :artist, :album, :mood, :album_art, :song_link, presence: true
+   validates :title, :artist, :album, :mood, :album_art, :song_link, :user_id, presence: true
    validates :title, uniqueness: {scope: :artist}
 end
