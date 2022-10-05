174023ae70e7a4e3528191ecf3e644c1f14f5083
ElijahBcodes <eli1inthesnow@gmail.com>
2022-10-05 14:59:11 -0700

    create method for song controller, strong params, rspec testing complete, test pass

--- spec/requests/songs_spec.rb
+++ spec/requests/songs_spec.rb
@@ -34,5 +34,37 @@ RSpec.describe "Songs", type: :request do
 
     end
   end
+
+  #----Create----
+  describe "POST /create" do
+    it 'creates a song for a user' do
+
+      song_params = {
+          song:{
+          title: "Our song",
+          artist: "You",
+          album: "Your album",
+          mood: "fury",
+          album_art: "image.com",
+          song_link: "youtube.com",
+          user_id: user.id
+        }
+      }
+
+      post '/songs', params: song_params
+
+      song = Song.last
+      p song
+
+      expect(response).to have_http_status(200)
+      expect(song['title']).to eq 'Our song'
+      expect(song['artist']).to eq 'You'
+      expect(song['album']).to eq 'Your album'
+      expect(song['mood']).to eq 'fury'
+      expect(song['album_art']).to eq 'image.com'
+      expect(song['song_link']).to eq  'youtube.com'
+    end
+  end
+
 end
 
