7e67204d88e1686c3ebbd9828a156a1eff5e2552
holdenprine <holdenprine404@gmail.com>
2022-10-05 11:23:30 -0700

    Navigation started, Sign Out functionality not working

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import {Nav, NavItem, Navbar} from 'reactstrap'
+import {NavLink} from 'react-router-dom'
 
-export const Navigation = () => {
-   return (
-      <div>Navigation</div>
-   )
+const Navigation = ({
+   logged_in,
+   current_user, 
+   sign_in_route, 
+   sign_out_route, 
+   new_user_route
+}) => {
+  return (
+   <>
+   <Navbar>
+      <Nav>
+         <NavItem>
+            <NavLink to = "/" className="nav-link">
+               Mood Music
+            </NavLink>
+         </NavItem>
+      </Nav>
+      <Nav>
+         <NavItem>
+            <NavLink to = "/" className="nav-link">
+               Who we are
+            </NavLink>
+         </NavItem>
+         {!logged_in && (
+            <NavItem>
+               <a href={sign_in_route} className="nav-link">
+                  Log in
+               </a>
+            </NavItem>
+         )}
+         {!logged_in && (
+            <NavItem>
+               <a href={new_user_route} className="nav-link">
+                  Sign Up
+               </a>
+            </NavItem>
+         )}
+         {logged_in && (
+            <NavItem>
+               <NavLink to = "/" className="nav-link">
+                  Added Songs
+               </NavLink>
+            </NavItem>
+         )}
+         {logged_in && (
+            <NavItem>
+               <a href={sign_out_route} className="nav-link">
+                  Sign Out
+               </a>
+            </NavItem>
+         )}
+      </Nav>
+   </Navbar>
+   </>
+  )
 }
 
 export default Navigation
