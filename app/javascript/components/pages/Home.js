c5fa8116728fcc6ca79959cfc825d1dec6d36da0
kellychan56 <kelly.chan568@gmail.com>
2022-10-12 10:36:17 -0700

    replaced youtube embed w/ media player

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,48 +1,15 @@
-import React, { useState } from 'react'
-import ReactPlayer from 'react-player'
-import { Button } from 'reactstrap'
-// import Player from '../components/Player'
-import { Media, Player, controls } from 'react-media-player'
-const { PlayPause, MuteUnmute } = controls
-
+import React from "react";
+import { Button } from "reactstrap";
 
 const Home = () => {
-   const [playing, setPlaying] = useState(false);
-
-   const handlePlayPause = () => {
-      setPlaying(!playing);
-   };
-
    return (
       <>
          <div>
-
             <h2>What are you in the mood for?</h2>
-            <Button href="/moods">
-               Pick A Mood
-            </Button>
-
+            <Button href="/moods">Pick A Mood</Button>
          </div>
-         <Media>
-        <div className="media">
-          <div className="media-player">
-            <Player src="http://www.youtube.com/embed/h3YVKTxTOgU" />
-          </div>
-          <div className="media-controls">
-            <PlayPause />
-            <MuteUnmute />
-          </div>
-        </div>
-      </Media>
-
-
-         <Button onClick={handlePlayPause}>
-            Play/pause
-         </Button>
-         <h1>{JSON.stringify(playing)}</h1>
-
       </>
-   )
-}
+   );
+};
 
-export default Home
+export default Home;
--- app/javascript/components/pages/SongShow.js
+++ app/javascript/components/pages/SongShow.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, useParams } from 'react-router-dom'
 import { Button, Card, CardBody, CardSubtitle, CardTitle } from 'reactstrap'
-import YoutubeEmbed from '../components/YoutubeEmbed'
+import { Media, Player, controls } from 'react-media-player';
 
 const SongShow = ({songs}) => {
    const { id } = useParams()
    const song = songs?.find(song => song.id === +id)
+   const { PlayPause, MuteUnmute } = controls;
+   const [playing, setPlaying] = useState(false)
 
    return (
       <>
@@ -14,7 +16,17 @@ const SongShow = ({songs}) => {
                Back to Songs
             </Button>
          </NavLink>
-         <YoutubeEmbed songs={songs} embedId={song.song_link} />
+         <Media>
+            <div className="media">
+               <div className="media-player">
+                  <Player src={song.song_link} />
+               </div>
+               <div className="media-controls">
+                  <PlayPause />
+                  <MuteUnmute />
+               </div>
+            </div>
+         </Media>
          <Card style={{width: '18rem'}}>
             <img alt="album art" src={song.album_art}/>
             <CardBody>
