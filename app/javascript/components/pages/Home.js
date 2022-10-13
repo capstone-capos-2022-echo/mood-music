8b07699ff6f93afdbbe04319d4f3d4e051f8422a
holdenprine <holdenprine404@gmail.com>
2022-10-13 16:44:42 -0700

    homepage styling updated

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -6,14 +6,14 @@ import { curvesPathName } from "tsparticles-path-curves";
 import ParticlesBackground from "../components/ParticlesBackground";
 
 const Home = () => {
-//   function particlesInit(tsParticles) {
-//     console.log("init", tsParticles);
-//     loadSeaAnemonePreset(tsParticles);
-//   }
-
   return (
     <div className="container">
       <div className="centered-button">
+        <h4 style={{
+          fontFamily: "Major Mono Display, monospace",
+          fontSize: "6.3rem",
+          textDecoration: "none"
+        }}>MÜD MÜZIK</h4>
         <h2>What are you in the mood for?</h2>
         <Button href="/moods">Pick A Mood</Button>
       </div>
