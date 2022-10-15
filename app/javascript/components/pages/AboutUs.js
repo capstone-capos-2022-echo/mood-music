382a85278010c34e8b8c88f1af48c1565afc49cf
holdenprine <holdenprine404@gmail.com>
2022-10-15 16:39:03 -0700

    Styling almost finished for about us page, images a little off

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -15,11 +15,12 @@ const AboutUs = () => {
                 }}
                 >
                     <img 
-                        className="about-photo"
+                        className="kelly-photo"
                         alt="Kelly"
                         src="../imageassets/kelly-photo.jpg"
                     />
                     <CardBody>
+                        <div className="kellys-card">
                         <CardTitle tag="h5" className='aboutus-name'>
                             Kelly
                         </CardTitle>
@@ -32,6 +33,8 @@ const AboutUs = () => {
                         <CardText className='aboutus-description'>
                         My name is Kelly, and I’m a full-stack web developer currently based in San Diego, CA. Although I’m currently in San Diego, my roots are in Oakland, CA in the Bay Area! I am a true homebody, so I enjoy staying at home, watching K-dramas, and building things from Pokémon nanoblocks to IKEA furniture to websites! My journey as a software developer has just begun, so join me  and see what I can do!
                         </CardText>
+                        </div>
+                        <div >
                             <span className='aboutus-icons-bg'>
                                 <a href="https://github.com/kellychan56" target="_blank">
                                     <AiFillGithub style={{width: '2em', height: '2em'}} />
@@ -43,6 +46,7 @@ const AboutUs = () => {
                             <span className='aboutus-icons-bg'>
                                 <a href="https://kellychan56.netlify.app/" target="_blank"><MdArticle style={{width: "2em", height: "2em"}}/></a>
                             </span>
+                            </div>
                     </CardBody>
                 </Card>
             </div>
@@ -68,7 +72,7 @@ const AboutUs = () => {
                             Tech Lead
                         </CardSubtitle>
                         <CardText className='aboutus-description'>
-                        I’m a web developer with a focus on Full Stack applications, but still exploring other technologies that catch my interest! Currently looking into API’s and backend frameworks. If you’re looking for a developer to add to your team, I’d love to hear from you!
+                        I’m a Web Developer with a focus on full stack applications, but still exploring other technologies that catch my interest! Currently working on building projects with API’s and backend frameworks. I enjoy all types of music, especially live. My hobbies include; snowboarding, surfing, hiking, rock climbing, trail ultra-runs, video games, and photography. If you’re looking for a developer to add to your team, I’d love to hear from you!
                         </CardText>
                             <span className='aboutus-icons-bg'>
                                 <a href="https://github.com/ElijahBcodes" target="_blank"><AiFillGithub style={{width: "2em", height: "2em"}}/></a>
@@ -104,7 +108,7 @@ const AboutUs = () => {
                             Design Lead
                         </CardSubtitle>
                         <CardText className='aboutus-description'>
-                            Hey! My name is Holden Prine and I'm a full stack developer based out of San Diego, California with a focus in backend and UX. I'm San Diego born and raised and when I'm not writing code I make music, go on hikes, and play unhealthy amounts of Overwatch and Monster Hunter. I'm new in my journey as a developer but love a challenge so if you're looking for a motivated developer feel free to connect!
+                            Hey! My name is Holden Prine and I'm a full stack developer based out of San Diego, California with a focus in backend and UX. I'm San Diego born and raised and when I'm not writing code I make music, go on hikes, and play unhealthy amounts of Overwatch and Monster Hunter. I'm new in my journey as a developer but love a challenge so if you're looking for a motivated problem solver feel free to connect!
                         </CardText>
                             <span className='aboutus-icons-bg'>
                                 <a className='icons' href="https://github.com/holdenprine" target="_blank"><AiFillGithub style={{width: "2em", height: "2em"}}/></a>
